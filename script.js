9b29c393e4cf6de855a8eddb797a1d5faecbba49
berlitzperu21-hue <berlitzperu21@gmail.com>
2025-12-01 15:54:53 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,18 @@
-const ninos = [
-    "ZOE","SOFIA","JUAN DIEGO","ALBA ROJAS","CATALINA",
-    "DANIELA","ALBA RINCON","LEONARDO","MATEO","GAEL"
-];
-
 const colores = [
     "Rojo", "Verde", "Azul", "Amarillo", "Naranja",
     "Morado", "Rosa", "Celeste", "Turquesa", "Dorado"
 ];
 
 function sortear() {
-    if (ninos.length === 0) {
+    if (colores.length === 0) {
         document.getElementById("resultado").innerHTML =
             "🎄 Todos los colores ya fueron asignados.";
         return;
     }
 
-    const idx = Math.floor(Math.random() * ninos.length);
-    const nombre = ninos.splice(idx, 1)[0];
+    const idx = Math.floor(Math.random() * colores.length);
     const color = colores.splice(idx, 1)[0];
 
     document.getElementById("resultado").innerHTML =
-        "🎁 " + nombre + " → " + color;
+        "🎁 Color asignado: <b>" + color + "</b>";
 }
